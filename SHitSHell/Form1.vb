7b3b3ba8cb6f7d1d754e31a8536f637deeec0b6c
WindowsLogic <admin@windowslogic.co.uk>
2022-10-04 00:02:08 +0100

    Added ability to add application to Startup folder.

--- SHitSHell/Form1.vb
+++ SHitSHell/Form1.vb
@@ -1,4 +1,7 @@
-﻿Public Class Form1
+﻿Imports IWshRuntimeLibrary
+Imports Shell32
+
+Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' thanks, shady ass website!! :)
@@ -55,19 +58,21 @@ errorHandler:
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        MsgBox("way too lazy to code this in so just copy this exe file to %userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup")
-    End Sub 'coming in 2.0 i swear
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        'Adds the application to the Startup folder.
 
+        Dim WshShell As WshShell = New WshShell()
+        Dim ShortcutPath As String = Environment.GetFolderPath(Environment.SpecialFolder.Startup)
+        Dim Shortcut As IWshShortcut = CType(WshShell.CreateShortcut(System.IO.Path.Combine(ShortcutPath, Application.ProductName) & ".lnk"), IWshShortcut)
+        Shortcut.TargetPath = Application.ExecutablePath
+        Shortcut.WorkingDirectory = Application.StartupPath
+        Shortcut.Description = "SHitSHell"
+        Shortcut.Save()
+        MsgBox("SHitSHell has been added to the Startup folder.", MsgBoxStyle.Information, "Information")
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         TextBox1.Text = TimeString
         TextBox2.Text = DateString
     End Sub 'i think this is like the only not jank piece of code
-
-    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
-
-    End Sub
 End Class
